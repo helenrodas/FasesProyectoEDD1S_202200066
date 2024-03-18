7aaae2bee2f512b08ee0d6988caa39e4ccf919e8
helenrodas <3030360790108@ingenieria.usac.edu.gt>
2024-03-18 17:32:26 -0600

    agrego matriz dispersa

--- Fase2/app/main.f90
+++ Fase2/app/main.f90
@@ -1,17 +1,29 @@
 program main
   use:: json_module
   use BTree_module
+  use matrix_m
 
   implicit none
-  integer :: option,size, i,dpiAsInt
+  integer :: option,size, i,dpiAsInt,io
   character(:), allocatable :: dpi, nombreCliente, password
+
+  character(len=100) :: filename
+  integer ::  j, id_capa, n_pixeles
+  integer :: fila, columna
+  character(:), allocatable :: color
+  
   
 
   type(BTree), pointer :: root => null()
+  type(matrix) :: m
   type(json_file) :: json
   type(json_core) :: jsonc
   type(json_value), pointer :: listPointer, animalPointer, attributePointer
+  type(json_value), pointer :: filaPointer, columnaPointer, colorPointer
+  type(json_value), pointer :: pixelPointer
+
   logical :: found
+  io = 1
   do 
     call menu_inicial()
     read(*, *) option
@@ -32,6 +44,7 @@ program main
   contains
 
   subroutine menu_inicial()
+    print *, " "
     print *, "...................................."
     print *, "  Bienvenido a Pixel Print Estudio  "
     print *, "...................................."
@@ -62,9 +75,24 @@ program main
     if (usuario == "admin" .and. password == "EDD2024") then
         call op_menuAdmin()
     else
-        print *, "El usuario no existe por el momento..."
+        call readCapas()
+        print *, "----Pruebas Matriz----"
+        call pruebaMatriz()
     end if
   end subroutine inicio_sesion
+
+
+  subroutine pruebaMatriz()
+    call m%insert(1,3,"#378a61")
+    call m%insert(8,4,"#8a377c")
+    call m%insert(5,0,"#641e16")
+    call m%insert(2,6,"#FFCC27")
+    call m%insert(9,7,"#8a6b37")
+    call m%insert(0,1,"#374b8a")
+
+    call m%print()
+    call m%graficar()
+  end subroutine
 
 
   subroutine menu_admin()
@@ -146,6 +174,60 @@ program main
     call inorder(root)
     call json%destroy()
 end subroutine readUsuarios
+
+subroutine readCapas()
+  
+  filename = 'capas.json'
+
+  print *, "---------------------------------------"
+  print *, "----- Capas -----"
+
+  call json%initialize()
+  call json%load(filename=filename)
+  call json%info('',n_children=size)
+  call json%get_core(jsonc)
+  call json%get('', listPointer, found)
+
+  ! Variables para almacenar datos
+  
+
+  do i = 1, size
+    call jsonc%get_child(listPointer, i, animalPointer, found)
+
+    ! Obtener el ID de la capa
+    call jsonc%get_child(animalPointer, 'id_capa', attributePointer, found)
+    call jsonc%get(attributePointer, id_capa)
+
+    ! Obtener los píxeles de la capa
+    call jsonc%get_child(animalPointer, 'pixeles', attributePointer, found)
+
+    ! Obtener el número de píxeles en la capa
+    call jsonc%info(attributePointer, n_children=n_pixeles)
+
+    do j = 1, n_pixeles
+        ! Obtener cada píxel individualmente
+        call jsonc%get_child(attributePointer, j, pixelPointer, found)
+
+        ! Obtener los atributos del píxel
+        call jsonc%get_child(pixelPointer, 'fila', filaPointer, found)
+        call jsonc%get(filaPointer, fila)
+
+        call jsonc%get_child(pixelPointer, 'columna', columnaPointer, found)
+        call jsonc%get(columnaPointer, columna)
+
+        call jsonc%get_child(pixelPointer, 'color', colorPointer, found)
+        call jsonc%get(colorPointer, color)
+
+        print *, "Capa:", id_capa, "Pixel (Fila, Columna):", fila, columna, "Color:" ,color
+        call m%insert(fila,columna,color)
+    end do
+end do
+
+  call json%destroy()
+end subroutine readCapas
+
+
+
 
 end program main
 
---
+++ Fase2/src/matrix.f90
@@ -0,0 +1,383 @@
+module matrix_m
+    implicit none
+    private
+    type :: node_val
+        private
+        logical :: exists = .false.
+        character(len=:), allocatable :: valor
+    end type node_val
+
+    type :: node
+        private
+        integer :: i,j
+        character(len=:), allocatable :: valor
+        type(node), pointer :: arriba => null()
+        type(node), pointer :: abajo => null()
+        type(node), pointer :: derecha => null()
+        type(node), pointer :: izquierda => null()
+    end type node
+
+    type, public :: matrix
+        private
+        type(node), pointer :: root => null()
+        integer :: width = 0
+        integer :: height = 0
+    contains
+        procedure :: insert
+        procedure :: insertarCabeceraFila
+        procedure :: insertarCabeceraColumna
+        procedure :: buscarFila
+        procedure :: buscarColumna
+        procedure :: existeNodo
+        procedure :: print
+        procedure :: imprimirEncabezadoColumnas
+        procedure :: obtenerValor
+        procedure :: graficar
+
+    end type matrix
+
+contains
+subroutine insert(self, i, j, valor)
+    class(matrix), intent(inout) :: self
+    integer, intent(in) :: i
+    integer, intent(in) :: j
+    character(len=*), intent(in) :: valor  ! Cambiar len=: por len=*
+
+    type(node), pointer :: nuevo
+    type(node), pointer :: fila
+    type(node), pointer :: columna
+
+    allocate(nuevo)
+    nuevo = node(i=i, j=j, valor=valor)
+
+    if(.not. associated(self%root)) then
+        allocate(self%root)
+        self%root = node(i=-1, j=-1)
+    end if
+
+    fila => self%buscarFila(j)
+    columna => self%buscarColumna(i)
+
+    if(i > self%width) self%width = i
+    if(j > self%height) self%height = j
+
+    if(.not. self%existeNodo(nuevo)) then
+        if(.not. associated(columna)) then
+            columna => self%insertarCabeceraColumna(i)
+        end if
+
+        if(.not. associated(fila)) then
+            fila => self%insertarCabeceraFila(j)
+        end if
+        call insertarEnColumna(nuevo, fila)
+        call insertarEnFila(nuevo, columna)
+    end if
+end subroutine insert
+
+
+    function insertarCabeceraColumna(self, i) result(nuevaCabeceraColumna)
+        class(matrix), intent(inout) :: self
+        integer, intent(in) :: i
+
+        type(node), pointer :: nuevaCabeceraColumna
+        allocate(nuevaCabeceraColumna)
+
+        nuevaCabeceraColumna = node(i=i, j=-1)
+        call insertarEnColumna(nuevaCabeceraColumna, self%root)
+    end function insertarCabeceraColumna
+    
+    function insertarCabeceraFila(self, j) result(nuevaCabeceraFila)
+        class(matrix), intent(inout) :: self
+        integer, intent(in) :: j
+
+        type(node), pointer :: nuevaCabeceraFila
+        allocate(nuevaCabeceraFila)
+
+        nuevaCabeceraFila = node(i=-1, j=j)
+        call insertarEnFila(nuevaCabeceraFila, self%root)
+    end function insertarCabeceraFila
+
+    subroutine insertarEnFila(nuevo, CabeceraFila)
+        type(node), pointer :: nuevo
+        type(node), pointer :: cabeceraFila !head 
+
+        type(node), pointer :: actual
+        actual => cabeceraFila
+
+        do while(associated(actual%abajo))
+            if(nuevo%j < actual%abajo%j) then
+
+                nuevo%abajo => actual%abajo
+                nuevo%arriba => actual
+                actual%abajo%arriba => nuevo
+                actual%abajo => nuevo
+                exit
+            end if
+            actual => actual%abajo
+        end do
+
+        if(.not. associated(actual%abajo)) then
+            actual%abajo => nuevo
+            nuevo%arriba => actual
+        end if
+    end subroutine insertarEnFila
+
+    subroutine insertarEnColumna(nuevo, CabeceraColumna)
+        type(node), pointer :: nuevo
+        type(node), pointer :: CabeceraColumna !head 
+
+        type(node), pointer :: actual
+        actual => CabeceraColumna
+
+        do while(associated(actual%derecha))
+            if(nuevo%i < actual%derecha%i) then
+
+                nuevo%derecha => actual%derecha
+                nuevo%izquierda => actual
+                actual%derecha%izquierda => nuevo
+                actual%derecha => nuevo
+                exit
+            end if
+            actual => actual%derecha
+        end do
+
+        if(.not. associated(actual%derecha)) then
+            actual%derecha => nuevo
+            nuevo%izquierda => actual
+        end if
+    end subroutine insertarEnColumna  
+
+    function buscarFila(self, j) result(actual)
+        class(matrix), intent(in) :: self
+        integer, intent(in) :: j
+
+        type(node), pointer :: actual
+        actual => self%root
+
+        do while(associated(actual)) 
+            if(actual%j == j) return
+            actual => actual%abajo
+        end do
+    end function buscarFila
+
+    function buscarColumna(self, i) result(actual)
+        class(matrix), intent(in) :: self
+        integer, intent(in) :: i
+
+        type(node), pointer :: actual
+        actual => self%root 
+        
+        do while(associated(actual))
+            if(actual%i == i) return
+            actual => actual%derecha
+        end do
+    end function buscarColumna
+
+    function existeNodo(self, nodo) result(existe)
+        class(matrix), intent(inout) :: self
+        type(node), pointer, intent(in) :: nodo
+
+        logical :: existe
+        type(node), pointer :: encabezadoFila
+        type(node), pointer :: columna
+        encabezadoFila => self%root
+        existe = .false.
+
+        do while(associated(encabezadoFila))
+            if(encabezadoFila%j == nodo%j) then
+                columna => encabezadoFila
+                do while(associated(columna)) 
+                    if(columna%i == nodo%i) then
+                        columna%valor = nodo%valor
+                        existe = .true.
+                        return
+                    end if
+                    columna => columna%derecha
+                end do
+                return
+            end if
+            encabezadoFila => encabezadoFila%abajo
+        end do
+        return
+    end function existeNodo
+
+    subroutine print(self)
+        class(matrix), intent(in) :: self
+        integer :: i
+        integer :: j
+        type(node), pointer :: aux
+        type(node_val) :: val
+        aux => self%root%abajo
+
+        call self%imprimirEncabezadoColumnas()
+
+        do j = 0, self%height
+            print *, ""
+            write(*, fmt='(I3)', advance='no') j
+
+            do i = 0, self%width
+                val = self%obtenerValor(i,j)
+                if(.not. val%exists) then
+                    write(*, fmt='(I3)', advance='no') 0
+                else
+                    write(*, fmt='(L3)', advance='no') val%valor
+                end if
+
+            end do
+        end do
+    end subroutine print
+
+    subroutine imprimirEncabezadoColumnas(self)
+        class(matrix), intent(in) :: self
+        integer :: i
+
+        do i=-1, self%width
+            write(*, fmt='(I3)', advance='no') i
+        end do
+    end subroutine imprimirEncabezadoColumnas
+
+
+    function obtenerValor(self, i, j) result(val)
+        class(matrix), intent(in) :: self
+        integer, intent(in) :: i
+        integer, intent(in) :: j
+
+        type(node), pointer :: cabeceraFila
+        type(node), pointer :: columna
+        type(node_val) :: val
+        cabeceraFila => self%root
+
+        do while(associated(cabeceraFila))
+            if(cabeceraFila%j == j) then
+                columna => cabeceraFila
+                do while(associated(columna)) 
+                    if(columna%i == i) then
+                        val%valor = columna%valor
+                        val%exists = .true.
+                        return
+                    end if
+                    columna => columna%derecha
+                end do
+                return
+            end if
+            cabeceraFila => cabeceraFila%abajo
+        end do
+        return
+    end function obtenerValor
+
+
+    subroutine graficar(self)
+        class(matrix), intent(in) :: self
+        
+        integer :: io
+        integer :: i
+        character(len=10) :: str_i
+        character(len=10) :: str_j
+        character(len=10) :: str_i_aux
+        character(len=10) :: str_j_aux
+        character(len=150) :: node_dec
+        character(len=20) :: nombre
+
+        character(len=100) :: comando
+        character(len=50) :: contenido
+        character(:), allocatable :: rank
+        character(:), allocatable :: conexion
+        character(:), allocatable :: conexionRev
+        type(node), pointer :: fila_aux
+        type(node), pointer :: columna_aux
+        io = 1
+        fila_aux => self%root
+        comando = "dot -Tpng ./matrix.dot -o ./matrix.png"
+
+        open(newunit=io, file="./matrix.dot")
+
+        write(io, *) "digraph Matrix {"
+        write(io, *) 'node[shape = "box"]'
+
+        do while (associated(fila_aux))
+            rank = "{rank=same"
+            columna_aux => fila_aux
+            do while(associated(columna_aux)) 
+                write(str_i, '(I10)') columna_aux%i + 1
+                write(str_j, '(I10)') columna_aux%j + 1
+                nombre = '"Nodo'//trim(adjustl(str_i))//'_'//trim(adjustl(str_j))//'"'
+                ! contenidoValor= columna_aux%valor
+                ! contenidoValor = "#FFCC33"
+                if (columna_aux%i == -1 .and. columna_aux%j == -1) then
+                    node_dec = trim(adjustl(nombre))//'[label = "root", group="'//trim(adjustl(str_i))//'"]'
+
+                else if(columna_aux%i == -1) then
+                    write(str_j_aux, '(I10)') columna_aux%j
+                    ! contenidoValor = "#FFCC27"
+                    contenido = trim(adjustl(str_j_aux))
+                    node_dec = trim(adjustl(nombre))//'[label = "'//trim(adjustl(contenido))
+                    node_dec = trim(adjustl(node_dec))//'", group="'//trim(adjustl(str_i))//'"]'
+                    
+                else if(columna_aux%j == -1) then
+                    ! contenidoValor = "#FFCC27"
+                    write(str_i_aux, '(I10)') columna_aux%i
+                    contenido = trim(adjustl(str_i_aux))
+                    node_dec = trim(adjustl(nombre))//'[label = "'//trim(adjustl(contenido))
+                    node_dec = trim(adjustl(node_dec))//'", group="'//trim(adjustl(str_i))//'"]'
+                        
+                else
+                    if(columna_aux%valor .NE. "" ) then
+                        contenido = columna_aux%valor
+                    else
+                        contenido = 'F'
+                    end if 
+                    node_dec = trim(adjustl(nombre))//'[label = "'//trim(adjustl(contenido))
+                    node_dec = trim(adjustl(node_dec))//'",  style = filled, fillcolor = "'//&
+                    & trim(contenido)//'" group="'// &
+                    & trim(adjustl(str_i))//'"]'
+                end if
+                write(io, *) node_dec
+
+                if(associated(columna_aux%derecha)) then
+                    conexion = '"Nodo'//trim(adjustl(str_i))//'_'//trim(adjustl(str_j))//'"->'
+
+                    write(str_i_aux, '(I10)') columna_aux%derecha%i + 1
+                    write(str_j_aux, '(I10)') columna_aux%derecha%j + 1
+
+                    conexion = conexion//'"Nodo'//trim(adjustl(str_i_aux))//'_'//trim(adjustl(str_j_aux))//'"'
+                    conexionRev = conexion//'[dir = back]'
+                    write(io, *) conexion
+                    write(io, *) conexionRev
+                end if
+
+                if(associated(columna_aux%abajo)) then
+                    conexion = '"Nodo'//trim(adjustl(str_i))//'_'//trim(adjustl(str_j))//'"->'
+
+                    write(str_i_aux, '(I10)') columna_aux%abajo%i + 1
+                    write(str_j_aux, '(I10)') columna_aux%abajo%j + 1
+
+                    conexion = conexion//'"Nodo'//trim(adjustl(str_i_aux))//'_'//trim(adjustl(str_j_aux))//'"'
+                    conexionRev = conexion//'[dir = back]'
+                    write(io, *) conexion
+                    write(io, *) conexionRev
+                end if
+
+                rank = rank//';"Nodo'//trim(adjustl(str_i))//'_'//trim(adjustl(str_j))//'"'
+                columna_aux => columna_aux%derecha
+            end do
+            rank = rank//'}'
+            write(io, *) rank
+
+            fila_aux => fila_aux%abajo
+        end do
+        write(io, *) "}"
+        close(io)
+        
+        call execute_command_line(comando, exitstat=i)
+
+        if ( i == 1 ) then
+            print *,""
+            print *, "Ocurrió un error al momento de crear la imagen"
+        else
+            print *,""
+            print *, "La imagen fue generada exitosamente"
+        end if
+    end subroutine graficar
+    
+
+end module matrix_m
