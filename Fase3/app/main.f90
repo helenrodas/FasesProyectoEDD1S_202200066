7d0972cc4a2a498b6abfa24194ac341b4acdf8c4
helenrodas <3030360790108@ingenieria.usac.edu.gt>
2024-04-28 12:55:21 -0600

    agrego funcion lectura de json sucursales y abb

--- Fase3/app/main.f90
+++ Fase3/app/main.f90
@@ -1,7 +1,20 @@
 program main
+  use:: json_module
+  use:: sucursales_module
   implicit none
-  ! use:: json_module
-integer :: option
+  
+  integer :: option,size
+  character(:), allocatable :: id, departamento,direccion ,password
+  integer :: i, s1, s2, distancia, imp_mantenimiento
+  type(json_file) :: json
+  type(json_core) :: jsonc
+  type(json_value), pointer :: listPointer, animalPointer, attributePointer,grafo_value
+  ! type(json_array), pointer :: grafo2
+  ! type(json_object), pointer :: item
+  logical :: found
+
+
+  type(sucursalABB) :: arbolSucursales
 
   do 
     call menu_inicial()
@@ -96,9 +109,9 @@ integer :: option
       
       select case(option)
       case(1)
-        print *, "carga sucursales"
+        call readSucursales("sucursales")
       case(2)
-        print *, "carga rutas"
+        ! call readRutas("rutas")
       case(3)
         print *, "menu salida"
         exit
@@ -120,5 +133,96 @@ integer :: option
     print *, "...................................."
     print *, "Ingrese el numero de la opcion deseada:"
   end subroutine menu_cargaArchivos
+
+
+  subroutine readSucursales(nombreArchivo)
+    integer :: i,idAsInt
+    character(len=*), intent(in)::nombreArchivo
+    character(len=100) :: filename 
+
+
+    print *, "------------Sucursales---------------"
+    call json%initialize()
+    filename = trim(nombreArchivo) // '.json'
+    call json%load(filename=filename)
+    call json%info('',n_children=size)
+    call json%get_core(jsonc)
+    call json%get('', listPointer, found)
+
+    do i = 1, size
+        call jsonc%get_child(listPointer, i, animalPointer, found)
+
+        call jsonc%get_child(animalPointer, 'id', attributePointer, found)
+        call jsonc%get(attributePointer, id)
+
+        call jsonc%get_child(animalPointer, 'departamento', attributePointer, found)
+        call jsonc%get(attributePointer, departamento)
+
+        call jsonc%get_child(animalPointer, 'direccion', attributePointer, found)
+        call jsonc%get(attributePointer, direccion)
+
+        call jsonc%get_child(animalPointer, 'password', attributePointer, found) 
+        call jsonc%get(attributePointer, password)
+
+        ! print *, "ID:",id
+        ! print *, "Departamento:",departamento
+        ! print *, "Direccion:",direccion
+        ! print *, "Password:",password
+        print *, "----------------------------"
+        read(id, *) idAsInt
+        call arbolSucursales%insert(idAsInt,departamento,direccion,password)
+
+    end do
+    call arbolSucursales%printABB()
+    call json%destroy()
+    print*,"Archivo sucursales leido exitosamente"
+end subroutine readSucursales
+
+
+
+! subroutine readRutas(nombreArchivo)
+!   character(len=*), intent(in) :: nombreArchivo
+!   character(len=100) :: filename
+!   integer :: i, size, found, num_edges
+
+!   ! Variables para almacenar los valores de cada borde
+!   integer :: s1, s2, distancia, imp_mantenimiento
+
+!   print *, "------------Grafo---------------"
+
+!   ! Inicializar la biblioteca json_module
+!   call json%initialize()
+
+!   ! Cargar el archivo JSON
+!   filename = trim(nombreArchivo) // '.json'
+!   call json%load(filename=filename)
+
+!   call json%get('grafo', grafo_value)
+!     call grafo_value%get_array(grafo2)
+
+!     do i = 1, grafo2%size()
+!         call grafo2%get(i, item)
+!         call item%get('s1', s1)
+!         call item%get('s2', s2)
+!         call item%get('distancia', distancia)
+!         call item%get('imp_mantenimiento', imp_mantenimiento)
+!     print *, "Edge ", i
+!       print *, "s1:", s1
+!       print *, "s2:", s2
+!       print *, "distancia:", distancia
+!       print *, "imp_mantenimiento:", imp_mantenimiento
+!       print *, "----------------------------"
+! end do
+
+
+!   ! Liberar recursos de json_module
+!   call json%destroy()
+
+!   print *, "Archivo grafo leído exitosamente"
+! end subroutine readRutas
+
+
+
+
 
 end program main
---
+++ Fase3/src/sucursalesABB.f90
@@ -0,0 +1,230 @@
+module sucursales_module
+    implicit none
+    private
+
+    type, public :: nodeSucursales
+        integer :: id
+        character(len=:), allocatable :: departamento,direccion,password
+        type(nodeSucursales), pointer :: right => null()
+        type(nodeSucursales), pointer :: left => null()
+    end type nodeSucursales
+
+    type, public :: sucursalABB
+        type(nodeSucursales), pointer :: root => null()
+
+    contains
+        procedure :: insert
+        procedure :: delete
+        procedure :: printABB
+        ! procedure :: buscarId
+        ! procedure :: existeIDABB
+    end type sucursalABB
+
+contains   
+
+
+! subroutine existeIDABB(self, id, encontrado)
+!     class(sucursalABB), intent(inout) :: self
+!     integer, intent(in) :: id
+!     logical, intent(out) :: encontrado
+!     type(nodeSucursales), pointer :: node
+
+!     node => existeNodoID(self%root, id)
+    
+!     if (associated(node)) then
+!       encontrado = .true.
+!     else
+!       encontrado = .false.
+!     end if
+
+!     contains
+
+!     recursive function existeNodoID(root, id) result(node)
+!         type(nodeSucursales), pointer :: root
+!         integer, intent(in) :: id
+!         type(nodeSucursales), pointer :: node
+
+!         if (associated(root)) then
+!             if (root%id == id) then
+!                 node => root
+!             else if (id < root%id) then
+!                 node => existeNodoID(root%left, id)
+!             else
+!                 node => existeNodoID(root%right, id)
+!             end if
+!         else
+!             node => null()
+!         end if
+!     end function existeNodoID
+! end subroutine existeIDABB
+
+! subroutine buscarId(self, id,fila,columa,color)
+!     class(sucursalABB), intent(inout) :: self
+!     integer, intent(in) :: id,fila,columa
+!     type(nodeSucursales), pointer :: node
+!     character(len=7) ::color
+
+!     node => existeNodo(self%root, id)
+    
+!     if (associated(node)) then
+!     !   print *, "Nodo encontrado"
+!       call insertarEnMatriz(node,fila,columa,color)
+!     else
+!     !   print *, "Nodo no encontrado"
+!     end if
+
+!     contains
+
+!     recursive function existeNodo(root, id) result(node)
+!         type(nodeSucursales), pointer :: root
+!         integer, intent(in) :: id
+!         type(nodeSucursales), pointer :: node
+
+!         if (associated(root)) then
+!             if (root%id == id) then
+!                 node => root
+!             else if (id < root%id) then
+!                 node => existeNodo(root%left, id)
+!             else
+!                 node => existeNodo(root%right, id)
+!             end if
+!         else
+!             node => null()
+!         end if
+!     end function existeNodo
+
+    
+    
+! end subroutine buscarId
+
+
+
+recursive subroutine printTree(root)
+    type(nodeSucursales), pointer, intent(inout) :: root
+
+    if (associated(root)) then
+        call printTree(root%left)
+
+        print*, "ID: ", root%id
+        print*, "Departamento: ", root%departamento
+        print*, "Direccion: ", root%direccion
+        print*, "Password: ", root%password
+        print*, "-------------------------"
+
+        call printTree(root%right)
+    end if
+end subroutine printTree
+
+subroutine printABB(self)
+    class(sucursalABB), intent(inout) :: self
+
+    if (associated(self%root)) then
+        call printTree(self%root)
+    else
+        print*, "El árbol está vacío."
+    end if
+end subroutine printABB
+
+
+
+  
+subroutine insert(self, id,departamento,direccion,password)
+    class(sucursalABB), intent(inout) :: self
+    integer, intent(in) :: id
+    character(len=*),intent(in)::departamento,direccion,password
+
+    if (.not. associated(self%root)) then
+        allocate(self%root)
+        self%root%id = id
+        self%root%departamento = departamento
+        self%root%direccion = direccion
+        self%root%password = password
+    else
+        call insertRec(self%root, id,departamento,direccion,password)
+    end if
+end subroutine insert
+
+recursive subroutine insertRec(root, id,departamento,direccion,password)
+    type(nodeSucursales), pointer, intent(inout) :: root
+    integer, intent(in) :: id
+    character(len=*),intent(in)::departamento,direccion,password
+    
+    if (id < root%id) then
+        if (.not. associated(root%left)) then
+            allocate(root%left)
+            root%left%id = id
+            root%left%departamento = departamento
+            root%left%direccion = direccion
+            root%left%password = password
+        else
+            call insertRec(root%left, id,departamento,direccion,password)
+        end if
+    else if (id > root%id) then
+        if (.not. associated(root%right)) then
+            allocate(root%right)
+            root%right%id = id
+            root%right%departamento = departamento
+            root%right%direccion = direccion
+            root%right%password = password
+        else
+            call insertRec(root%right,id,departamento,direccion,password)
+        end if
+    end if
+end subroutine insertRec
+
+    subroutine delete(self, id)
+        class(sucursalABB), intent(inout) :: self
+        integer, intent(inout) :: id
+    
+        self%root => deleteRec(self%root, id)
+    end subroutine delete
+
+    recursive function deleteRec(root, id) result(res)
+        type(nodeSucursales), pointer :: root
+        integer, intent(in) :: id
+        type(nodeSucursales), pointer :: res
+        type(nodeSucursales), pointer :: temp
+
+        if (.not. associated(root)) then
+            res => root
+            return
+        end if
+
+        if (id < root%id) then
+            root%left => deleteRec(root%left, id)
+        else if (id > root%id) then
+            root%right => deleteRec(root%right, id)
+        else
+            if (.not. associated(root%left)) then
+                temp => root%right
+                deallocate(root)
+                res => temp
+                return
+            else if (.not. associated(root%right)) then
+                temp => root%left
+                deallocate(root)
+                res => temp
+                return
+            else
+                call getMajorOfMinors(root%left, temp)
+                root%id = temp%id
+                root%left => deleteRec(root%left, temp%id)
+            end if
+        end if
+
+        res => root
+    end function deleteRec
+
+    recursive subroutine getMajorOfMinors(root, major)
+        type(nodeSucursales), pointer :: root, major
+        if (associated(root%right)) then
+            call getMajorOfMinors(root%right, major)
+        else
+            major => root
+        end if
+    end subroutine getMajorOfMinors
+
+
+
+
+end module sucursales_module
